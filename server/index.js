72d83883fbfed925762107f94105151dd410a560
sohini-sotbella <sohini@sotbella.com>
2026-07-31 16:35:03 +0530

    change port 5001 to 5002

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const Contact  = require('./models/Contact');
 const { sendContactEmail } = require('./services/emailService');
 
 const app  = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 
 // ── Middleware ────────────────────────────────────────────────
 app.use(cors());
